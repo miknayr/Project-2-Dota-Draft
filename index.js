355af91979e8a6d7a24d3ca2f21f2829300a843f
miknayr <teamry4n@gmail.com>
2021-06-09 13:18:50 -0700

    6/9 1pm update;

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const express = require('express')
 const layouts = require('express-ejs-layouts')
+const axios = require('axios'); 
 const app = express()
 const PORT = process.env.PORT
 app.set('view engine', 'ejs')
@@ -12,15 +13,32 @@ app.use(layouts)
 app.use(express.static(__dirname+'/public'))
 
 // get '/' - show a form that lets the user search for a location
+// app.get('/', (req, res) => {
+//   // res.send('Dota heroes Coming soon.')
+//   res.render('home.ejs')
+// })
 app.get('/', (req, res) => {
-  // res.send('Dota heroes Coming soon.')
-  res.render('home.ejs')
+  let dotaUrl = 'https://api.opendota.com/api/heroStats';
+  // Use request to call the API
+  axios.get(dotaUrl).then(apiResponse => {
+    let dotaData = apiResponse.data;
+    res.render('index', { dotaData: dotaData });
+  })
 })
-app.get('/heroes/:hero_id', (req, res) => {})
 
-app.get('/teams', (req, res) => {})
-app.get('/teams/:team_id', (req, res) => {})
-app.get('/teams/compare', (req, res) => {})
+
+// app.get('/heroes/:hero_id', (req, res) => {
+//   axios.get(https://api.opendota.com/api/heroes)
+//   .then((response) => {
+//     console.log(req.params.id)
+//     res.render('detail', {details: response.data});
+//   })
+//   .catch(err => {console.log(err)})
+// })
+
+// app.get('/teams', (req, res) => {})
+// app.get('/teams/:team_id', (req, res) => {})
+// app.get('/teams/compare', (req, res) => {})
 
 
 
