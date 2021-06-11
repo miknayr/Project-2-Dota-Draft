7fc1c479d58360784bb5bf92fd731a63ca3cdbfe
miknayr <teamry4n@gmail.com>
2021-06-11 13:33:17 -0700

    updated the views index

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ const db = ('./models');
 app.use(layouts)
 app.use(express.static(__dirname+'/public'))
 
-// get '/' - show a form that lets the user search for a location
-// app.get('/', (req, res) => {
-//   // res.send('Dota heroes Coming soon.')
-//   res.render('home.ejs')
-// })
 app.get('/', (req, res) => {
   let dotaUrl = 'https://api.opendota.com/api/heroStats';
   // Use request to call the API
@@ -40,13 +35,6 @@ app.get('/:id', (req, res) => {
       }
     }
 
-    // let hero = apiResponse.data.filter(x => { return x.hero_id == 10}) // <-- talk to kenny 
-    // let trueId = (req.params.id-1)
-    // if (req.params.id > 24){
-    //   trueId -= 1
-    // }
-    // console.log(apiResponse.data[x].hero_id)
-    // console.log(hero)
     res.render('heroes/hero-stats', {dotaData: hero});
   })
   .catch(err => {console.log(err)})
@@ -57,18 +45,6 @@ app.get('/:id', (req, res) => {
 // app.get('/teams/:team_id', (req, res) => {})
 // app.get('/teams/compare', (req, res) => {})
 
-// const team = existingTeam || [];
-
-// function add_hero(hero) {
-//   // prevent team size from exceeding 5
-//   if (team.length >= 5) {
-//     // do nothing
-//     return;
-//   }
-
-//   team.push(hero);
-//   render();
-// }
 
 
 
